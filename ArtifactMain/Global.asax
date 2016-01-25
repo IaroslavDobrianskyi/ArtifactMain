98acb0bb64e392a74635c4685c29008a7a2e63b1
Iaroslav Dobrianskyi <uranleo@gmail.com>
2016-01-25 21:33:30 +0200

    initial commit

---
+++ ArtifactMain/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArtifactMain.MvcApplication" Language="C#" %>
---
+++ ArtifactMain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArtifactMain.Startup))]
+namespace ArtifactMain
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
